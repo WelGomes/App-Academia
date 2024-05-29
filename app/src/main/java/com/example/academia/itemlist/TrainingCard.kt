8dcbd58dd30f42c05f85fff2831ed7ddee30a513
Welbert <welbert.gomes12@gmail.com>
2024-05-29 15:25:12 -0300

    att1

--- app/src/main/java/com/example/academia/itemlist/TrainingCard.kt
+++ app/src/main/java/com/example/academia/itemlist/TrainingCard.kt
@@ -1,5 +1,6 @@
 package com.example.academia.itemlist
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -13,6 +14,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.fastForEachIndexed
 import androidx.constraintlayout.compose.ConstraintLayout
 import coil.compose.rememberImagePainter
 import com.example.academia.model.Training
@@ -21,106 +23,112 @@ import com.example.academia.ui.theme.ORANGE
 
 @Composable
 fun TrainingCard(
-    training: Training,
+    training: Training
 ) {
 
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(20.dp)
-    ) {
-        
-        ConstraintLayout(
+    val firstExercise = training.exercise?.firstOrNull()
+
+    Column {
+        Card(
             modifier = Modifier
+                .fillMaxWidth()
                 .padding(20.dp)
-                .height(500.dp)
+                .height(200.dp)
         ) {
-            val (img, txtNameTraining, txtDescription, txtDate, txtNameExercises, txtObservation) = createRefs()
 
-            val firstExercise = training.exercise?.firstOrNull()
-
-            Text(
-                text = training.name.toString(),
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                color = ORANGE,
+            ConstraintLayout(
                 modifier = Modifier
-                    .constrainAs(txtNameTraining) {
-                        start.linkTo(parent.start, margin = 50.dp)
-                        end.linkTo(parent.end, margin = 50.dp)
-                    }
-            )
+                    .padding(20.dp)
+            ) {
+                val (txtNameTraining, txtDescription, txtDate) = createRefs()
 
-            Text(
-                text = " - ${training.date.toString()}",
-                fontSize = 15.sp,
-                fontWeight = FontWeight.Bold,
-                color = BLACK,
-                modifier = Modifier
-                    .constrainAs(txtDate) {
-                        start.linkTo(txtNameTraining.start, margin = 20.dp)
-                    }
-            )
-
-            Text(
-                text = training.description.toString(),
-                fontSize = 15.sp,
-                fontWeight = FontWeight.Bold,
-                color = BLACK,
-                modifier = Modifier
-                    .constrainAs(txtDescription) {
-                        top.linkTo(txtNameTraining.top, margin = 20.dp)
-                        start.linkTo(parent.start, margin = 50.dp)
-                        end.linkTo(parent.end, margin = 50.dp)
-                    }
-            )
-
-            Text(
-                text = firstExercise?.name.toString(),
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                color = ORANGE,
-                modifier = Modifier
-                    .constrainAs(txtNameExercises) {
-                        top.linkTo(txtDescription.top, margin = 20.dp)
-                        start.linkTo(parent.start, margin = 50.dp)
-                        end.linkTo(parent.end, margin = 50.dp)
-                    }
-            )
-
-            if(firstExercise != null) {
-                androidx.compose.foundation.Image(
-                    painter = rememberImagePainter(
-                        data = firstExercise.imageUri,
-                        builder = {
-                            crossfade(true)
-                        }
-                    ),
-                    contentDescription = null,
+                Text(
+                    text = "Date - ${training.date.toString()}",
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = BLACK,
                     modifier = Modifier
-                        .clip(CircleShape)
-                        .size(60.dp)
-                        .constrainAs(img) {
-                            top.linkTo(txtNameExercises.top, margin = 10.dp)
+                        .constrainAs(txtDate) {
+
                         }
                 )
 
-            }
+                Text(
+                    text = training.name.toString(),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = ORANGE,
+                    modifier = Modifier
+                        .constrainAs(txtNameTraining) {
 
-            Text(
-                text = firstExercise?.observation.toString(),
-                fontSize = 15.sp,
-                fontWeight = FontWeight.Bold,
-                color = BLACK,
-                modifier = Modifier
-                    .constrainAs(txtObservation) {
-                        top.linkTo(img.top, margin = 20.dp)
-                        start.linkTo(parent.start, margin = 50.dp)
-                        end.linkTo(parent.end, margin = 50.dp)
+                        }
+                )
+
+                Text(
+                    text = training.description.toString(),
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = BLACK,
+                    modifier = Modifier
+                        .constrainAs(txtDescription) {
+
+                        }
+                )
+
+
+
+            }
+        }
+
+        training.exercise?.forEach() { index->
+            Card {
+                ConstraintLayout {
+
+                    val (img, txtNameExercises, txtObservation) = createRefs()
+
+                    Text(
+                        text = index.name.toString(),
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = ORANGE,
+                        modifier = Modifier
+                            .constrainAs(txtNameExercises) {
+
+                            }
+                    )
+
+                    if (firstExercise != null) {
+                        androidx.compose.foundation.Image(
+                            painter = rememberImagePainter(
+                                data = index.imageUri,
+                                builder = {
+                                    crossfade(true)
+                                }
+                            ),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .clip(CircleShape)
+                                .size(60.dp)
+                                .constrainAs(img) {
+
+                                }
+                        )
+
                     }
-            )
+
+                    Text(
+                        text = index.observation.toString(),
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = BLACK,
+                        modifier = Modifier
+                            .constrainAs(txtObservation) {
+
+                            }
+                    )
+                }
+            }
         }
 
     }
-
 }
